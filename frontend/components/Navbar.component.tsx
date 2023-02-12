539d9425811b2853a6f8b16ce4ce413c414e43aa
Antony Chiramel <antonychiramel07@gmail.com>
2023-02-12 10:09:14 +0000

    home page boilerplate set

--- frontend/components/Navbar.component.tsx
+++ frontend/components/Navbar.component.tsx
@@ -29,10 +29,9 @@ function Navbar() {
   const { darkTheme, setDarkTheme } = useDarkTheme((state) => state);
 
   const router = useRouter();
-  console.log("router: ", router);
 
   return (
-    <div className="relative">
+    <div className="relative z-50">
       <nav className="max-w-7xl mx-auto px-6 py-7 flex items-center justify-between">
         <motion.div
           initial={{
@@ -81,6 +80,105 @@ function Navbar() {
             duration={0.8}
           />
         </motion.div>
+
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          className="hidden md:block"
+        >
+          <ul className="flex items-center gap-14">
+            <Link href="/">
+              <li
+                className={`${
+                  router.asPath === "/"
+                    ? "text-red-500 border-b-2 border-red-500 dark:text-amber-500 dark:border-amber-500"
+                    : "text-black dark:text-white"
+                } transition duration-1000`}
+              >
+                Home
+              </li>
+            </Link>
+            <Link href="/about">
+              <li
+                className={`${
+                  router.asPath === "/about"
+                    ? "text-red-500 border-b-2 border-red-500 dark:text-amber-500 dark:border-amber-500"
+                    : "text-black dark:text-white"
+                } transition duration-1000`}
+              >
+                About
+              </li>
+            </Link>
+            <Link href="/work">
+              <li
+                className={`${
+                  router.asPath === "/work"
+                    ? "text-red-500 border-b-2 border-red-500 dark:text-amber-500 dark:border-amber-500"
+                    : "text-black dark:text-white"
+                } transition duration-1000`}
+              >
+                Work
+              </li>
+            </Link>
+            <Link href="/contact">
+              <li
+                className={`${
+                  router.asPath === "/contact"
+                    ? "text-red-500 border-b-2 border-red-500 dark:text-amber-500 dark:border-amber-500"
+                    : "text-black dark:text-white"
+                } transition duration-1000`}
+              >
+                Contact
+              </li>
+            </Link>
+            {darkTheme ? (
+              <button
+                className="flex items-center gap-1 text-black dark:text-white"
+                onClick={() => setDarkTheme(false)}
+              >
+                <span>Light</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                </svg>
+              </button>
+            ) : (
+              <button
+                className="flex items-center gap-1 text-black dark:text-white"
+                onClick={() => setDarkTheme(true)}
+              >
+                <span>Dark</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            )}
+          </ul>
+        </motion.div>
       </nav>
 
       {isSidebarOpen && (
@@ -97,7 +195,7 @@ function Navbar() {
             stiffness: 1000,
             damping: 1000,
           }}
-          className="absolute left-0 right-0 top-0 transition duration-1000 bg-red-500 dark:bg-amber-500"
+          className="absolute left-0 right-0 top-0 z-50 transition duration-1000 bg-red-500 dark:bg-amber-500"
         >
           <motion.div
             variants={variants}
--- frontend/modules/Home.module.tsx
+++ frontend/modules/Home.module.tsx
@@ -1,7 +1,21 @@
 import Head from "next/head";
 import { motion } from "framer-motion";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import useDarkTheme from "../store/useDarkTheme.store";
 
 function Home() {
+  const { darkTheme } = useDarkTheme((state) => state);
+
+  const [text] = useTypewriter({
+    words: [
+      "I am a web developer, serverless enthusiast, and a wannabe designer.",
+      "You've found the right guy. What are you waiting for?",
+      "Let's work together!",
+    ],
+    loop: true,
+    delaySpeed: 3000,
+  });
+
   return (
     <>
       <Head>
@@ -15,11 +29,22 @@ function Home() {
         animate={{ y: "0%", opacity: 1 }}
         transition={{ duration: 0.75, ease: "easeOut" }}
         exit={{ y: "-100%", opacity: 0 }}
-        className="max-w-7xl mx-auto"
+        className="max-w-7xl mx-auto px-6 py-7"
       >
-        <h1 className="transition duration-1000 text-red-500 text-3xl dark:text-amber-500">
-          Home
-        </h1>
+        <div className="mt-20 max-w-xl xl:max-w-3xl">
+          <h1 className="transition duration-1000 text-red-500 text-5xl md:text-6xl xl:text-7xl dark:text-amber-500">
+            Hello.
+          </h1>
+          <h1 className="transition duration-1000 text-black text-3xl md:text-4xl xl:text-5xl dark:text-white">
+            <span>
+              {text}
+              <Cursor cursorColor={darkTheme ? "#ffffff" : "#000000"} />
+            </span>
+          </h1>
+        </div>
+        <button className="transition duration-1000 mt-10 border rounded px-4 py-3 border-black text-black dark:border-white dark:text-white">
+          Download resume
+        </button>
       </motion.main>
     </>
   );
